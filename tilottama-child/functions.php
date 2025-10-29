cdd8df1c231fd6d5104ef0ede902cf33ea884c45
Abhra1270 <netflixabhra@gmail.com>
2025-10-29 06:45:15 -0700

    Add Tilottama child theme functions.php
    Enqueue parent and child theme styles in functions.php

---
+++ tilottama-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', function() {
+    // Enqueue parent theme style
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Enqueue child theme style
+    wp_enqueue_style( 'tilottama-child-style', get_stylesheet_directory_uri() . '/style.css', [ 'parent-style' ], wp_get_theme()->get( 'Version' ) );
+});
