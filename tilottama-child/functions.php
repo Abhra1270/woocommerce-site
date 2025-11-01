69f736493789be5370760b289571f6addc65ab00
Abhra1270 <netflixabhra@gmail.com>
2025-11-01 12:48:31 +0530

    Add landing hero component demo and shortcode

--- tilottama-child/functions.php
+++ tilottama-child/functions.php
@@ -1,7 +1,9 @@
 <?php
-add_action( 'wp_enqueue_scripts', function() {
+add_action( 'wp_enqueue_scripts', function () {
     // Enqueue parent theme style
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     // Enqueue child theme style
     wp_enqueue_style( 'tilottama-child-style', get_stylesheet_directory_uri() . '/style.css', [ 'parent-style' ], wp_get_theme()->get( 'Version' ) );
-});
+} );
+
+require_once get_stylesheet_directory() . '/tilo-landing-hero.php';
